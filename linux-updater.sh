28bb3c3028d961e8c5d4cb8d7ffc8435864c2a78
Jason Prime <600613.m6@gmail.com>
2023-07-04 23:21:41 +0200

    Version 1.0.0
    The simple way

---
+++ linux-updater.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+########################################################################
+# Developer: Brainhub24
+# Project: Linux Updater Script
+# Version: 1.0
+# Description: The Linux Updater Script is a robust and efficient
+#              tool meticulously crafted to streamline package
+#              management on diverse Linux distributions. It automates
+#              the update, upgrade, autoclean, and autoremove processes
+#              with precision, ensuring your system remains secure,
+#              up-to-date, and optimized.
+#
+# Changelog:
+#   - Version 1.0 (2023-07-04):
+#     * Initial release of the Linux Updater Script (The simple way!).
+########################################################################
+
+sudo apt update && sudo apt upgrade -y
